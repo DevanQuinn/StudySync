098e499676e1021b392cd1b35fe0c72fd881c4c0
Devan Quinn <62554326+DevanQuinn@users.noreply.github.com>
2024-02-28 18:12:31 -0500

    remove env variable support

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: import.meta.env.REACT_APP_apiKey,
-	authDomain: import.meta.env.REACT_APP_authDomain,
-	projectId: import.meta.env.REACT_APP_projectId,
-	storageBucket: import.meta.env.REACT_APP_storageBucket,
-	messagingSenderId: import.meta.env.REACT_APP_messagingSenderId,
-	appId: import.meta.env.REACT_APP_appId,
+	apiKey: 'AIzaSyAOLFu9q6gvdcDoOJ0oPuQKPgDyOye_2uM',
+	authDomain: 'studysync-3fbd7.firebaseapp.com',
+	projectId: 'studysync-3fbd7',
+	storageBucket: 'studysync-3fbd7.appspot.com',
+	messagingSenderId: '885216959280',
+	appId: '1:885216959280:web:917a7216776b36e904c6f5',
+	measurementId: 'G-TS13EWHRMB',
 };
 
 // Initialize Firebase
