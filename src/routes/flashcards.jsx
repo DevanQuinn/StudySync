24851e32ed262698a299f507651dbec911223b2e
Sai Monish <tsaimonish@gmail.com>
2024-03-05 14:09:26 -0500

    Small change

--- src/routes/flashcards.jsx
+++ src/routes/flashcards.jsx
@@ -1,165 +1,144 @@
-import React, { useEffect } from 'react';
-import {
-	Box,
-	Typography,
-	TextField,
-	Button,
-	Container,
-	CssBaseline,
-	CircularProgress,
-} from '@mui/material';
-import {
-	query,
-	getFirestore,
-	collection,
-	getDocs,
-	doc,
-	addDoc,
-	deleteDoc,
-} from 'firebase/firestore';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import app from '../firebase';
-import FlashCard from '../components/FlashCard';
+import React from 'react';
+import { Box, Typography, TextField, Button } from '@mui/material';
 
-const Flashcards = () => {
-	const [flashcardList, setFlashcardList] = React.useState([]);
-	const [newQuestion, setNewQuestion] = React.useState('');
-	const [newAnswer, setNewAnswer] = React.useState('');
-	const [newImage, setNewImage] = React.useState(null);
-	const [newAudio, setNewAudio] = React.useState(null);
-	const [user, setUser] = React.useState();
-	const [loading, setLoading] = React.useState(true);
+const Flashcard = () => {
 
-	const db = getFirestore(app);
-	const col = user
-		? collection(db, `flashcards/${user?.uid}/flashcards`)
-		: null;
+    const [flashcards, setFlashcards] = React.useState([]);
+    const [newQuestion, setNewQuestion] = React.useState('');
+    const [newAnswer, setNewAnswer] = React.useState('');
+    const [newImage, setNewImage] = React.useState(null);
+    const [newAudio, setNewAudio] = React.useState(null);
+    const [flipped, setFlipped] = React.useState(false);
 
-	const fetchCards = async () => {
-		if (!col) return;
-		const q = query(col);
-		const snapshot = await getDocs(q);
-		const queriedFlashcards = [];
-		snapshot.forEach(card => {
-			const data = card.data();
-			data.id = card.id;
-			queriedFlashcards.push(data);
-		});
-		setFlashcardList(queriedFlashcards);
-		setLoading(false);
-	};
+    const addFlashcard = (event) => {
+        /* avoids page reload when card submitted */
+        event.preventDefault();
 
-	useEffect(() => {
-		const auth = getAuth();
-		onAuthStateChanged(auth, user => {
-			if (user) setUser(user);
-			else setLoading(false);
-		});
-	}, []);
+        const newFlashcard = {
+            question: newQuestion,
+            answer: newAnswer,
+            image: newImage,
+            audio: newAudio
+        };
 
-	useEffect(() => {
-		fetchCards();
-	}, [user]);
+        setFlashcards([...flashcards, newFlashcard]);
 
-	const uploadCard = async flashcard => {
-		await addDoc(col, flashcard);
-		fetchCards();
-	};
-	const addFlashcard = event => {
-		/* avoids page reload when card submitted */
-		event.preventDefault();
+        setNewQuestion('');
+        setNewAnswer('');
+        setNewImage(null);
+        setNewAudio(null);
+    };
 
-		const newFlashcard = {
-			question: newQuestion,
-			answer: newAnswer,
-			image: newImage,
-			audio: newAudio,
-		};
+    const deleteFlashcard = (index) => {
+        const updatedFlashcards = [...flashcards];
+        updatedFlashcards.splice(index, 1);
+        setFlashcards(updatedFlashcards);
+    };
 
-		uploadCard(newFlashcard);
+    const toggleFlip = () => {
+        setFlipped(!flipped);
+    }
 
-		setNewQuestion('');
-		setNewAnswer('');
-		setNewImage(null);
-		setNewAudio(null);
-	};
+    return (
+        <Box sx={{ maxWidth: 600 }}>
+            <Typography component="h1" variant="h5" align="center">
+                Flashcards
+            </Typography>
 
-	const deleteFlashcard = id => {
-		deleteDoc(doc(db, `flashcards/${user?.uid}/flashcards`, id)).then(() => {
-			fetchCards();
-		});
-	};
+            <Box component="form" onSubmit={addFlashcard} sx={{ mt: 2 }}>
+                <TextField
+                    label="Question"
+                    value={newQuestion}
+                    onChange={(e) => setNewQuestion(e.target.value)}
+                    fullWidth
+                    margin="normal"
+                />
 
-	// if (!user) return <div>Not signed in</div>;
+                <TextField
+                    label="Answer"
+                    multiline
+                    rows={4}
+                    value={newAnswer}
+                    onChange={(e) => setNewAnswer(e.target.value)}
+                    fullWidth
+                    margin="normal"
+                />
 
-	return (
-		<Container component='main' maxWidth='xs' sx={{ mt: 10 }}>
-			<CssBaseline />
-			<Box sx={{ maxWidth: 600 }}>
-				<Typography component='h1' variant='h5' align='center'>
-					Flashcards
-				</Typography>
+                <label>
+                    Image:
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={(e) => setNewImage(e.target.files[0])}
+                    />
+                </label>
 
-				<Box component='form' onSubmit={addFlashcard} sx={{ mt: 2 }}>
-					<TextField
-						label='Question'
-						value={newQuestion}
-						onChange={e => setNewQuestion(e.target.value)}
-						fullWidth
-						margin='normal'
-					/>
+                <label>
+                    Audio:
+                    <input
+                        type="file"
+                        accept="audio/*"
+                        onChange={(e) => setNewAudio(e.target.files[0])}
+                    />
+                </label>
 
-					<TextField
-						label='Answer'
-						multiline
-						rows={4}
-						value={newAnswer}
-						onChange={e => setNewAnswer(e.target.value)}
-						fullWidth
-						margin='normal'
-					/>
+                <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+                    Add Flashcard
+                </Button>
+            </Box>
 
-					{/* <label>
-						Image:
-						<input
-							type='file'
-							accept='image/*'
-							onChange={e => setNewImage(e.target.files[0])}
-						/>
-					</label>
-
-					<label>
-						Audio:
-						<input
-							type='file'
-							accept='audio/*'
-							onChange={e => setNewAudio(e.target.files[0])}
-						/>
-					</label> */}
-
-					<Button type='submit' variant='contained' sx={{ mt: 2 }}>
-						Add Flashcard
-					</Button>
-				</Box>
-
-				{loading ? (
-					<Box sx={{ mt: 4 }}>
-						<CircularProgress />
-					</Box>
-				) : (
-					<Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 4 }}>
-						{flashcardList.map(card => (
-							<FlashCard
-								data={card}
-								deleteFlashcard={deleteFlashcard}
-								key={card.id}
-							/>
-						))}
-					</Box>
-				)}
-			</Box>
-		</Container>
-	);
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 4 }}>
+                {flashcards.map((flashcard, index) => (
+                    <Box
+                        key={index}
+                        onClick={toggleFlip}
+                        border={1}
+                        borderColor="grey"
+                        borderRadius={2}
+                        p={2}
+                        display="flex"
+                        flexDirection="column"
+                        alignItems="center"
+                    >
+                        {flipped ? (
+                            <>
+                                <Typography>{flashcard.answer}</Typography>
+                                <Button onClick={toggleFlip}>Show Question</Button>
+                            </>
+                        ) : (
+                            <>
+                                <Typography variant="h6">
+                                    {flashcard.question}
+                                </Typography>
+                                {(() => {
+                                    if (flashcard.image) {
+                                        return <img src={URL.createObjectURL(flashcard.image)} alt="" />;
+                                    }
+                                })()}
+                                {(() => {
+                                    if (flashcard.audio) {
+                                        return (
+                                            <audio controls>
+                                                <source
+                                                    src={URL.createObjectURL(flashcard.audio)}
+                                                    type="audio/mpeg"
+                                                />
+                                                Your browser does not support the audio element.
+                                            </audio>
+                                        );
+                                    }
+                                })()}
+                                <Button onClick={toggleFlip}>Reveal Answer</Button>
+                            </>
+                        )}
+                        <Button onClick={() => deleteFlashcard(index)}>
+                            Delete
+                        </Button>
+                    </Box>
+                ))}
+            </Box>
+        </Box>
+    );
 };
 
-export default Flashcards;
+export default Flashcard;
