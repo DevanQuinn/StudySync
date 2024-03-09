0114a7746d5cda443076c656b9ca94812ee13c75
JFleming132 <josephflemingvi@gmail.com>
2024-03-09 15:56:49 -0500

    completelymodularized tasklists. Next, make them collapsable.

--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import './Tasklist.css';
-import Draggable from 'react-draggable';
 import { Button } from '@mui/material';
 
 function Task({ task, taskID, tasklistID, completeTask, removeTask }) {
@@ -35,19 +34,23 @@ function Tasklist({title, deletefunc, id, tasksProp, addtaskfunc, deletetaskfunc
             <div className="header">{title}</div>
                 <div>
                     <div className="tasks">
-                        {tasksProp.map((task) => { //not properly mapping
-                            console.log("mapping task: ");
-                            console.log(task);
-                            return (
-                            <Task
-                            task={task}
-                            tasklistID={id}
-                            completeTask={() => completetaskfunc(id, task.taskID)}
-                            removeTask={() => deletetaskfunc(id, task.taskID)}
-                            key={task.taskID}
-                            />
-                        )}
-                        )}
+                        <input id="collapsible" className="toggle" type="checkbox"></input>
+                        <label for="collapsible" className="lbl-toggle">Collapse/Expand</label>
+                        <div className="collapsible-content">
+                            {tasksProp.map((task) => { //not properly mapping
+                                console.log("mapping task: ");
+                                console.log(task);
+                                return (
+                                <Task
+                                task={task}
+                                tasklistID={id}
+                                completeTask={() => completetaskfunc(id, task.taskID)}
+                                removeTask={() => deletetaskfunc(id, task.taskID)}
+                                key={task.taskID}
+                                />
+                            )}
+                            )}
+                        </div>
                     </div>
                     <>{tasksProp.length} tasks left!</>
                     <div className="create-task" >
